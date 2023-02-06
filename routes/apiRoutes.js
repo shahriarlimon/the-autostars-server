64b85dcaeea2a3bf8079d6ed8f338e3204d0fd56
shahriar limon <shahriarwebdev@gmail.com>
2023-02-06 16:49:41 +0600

    added error middleware

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const productRoutes = require("../routes/productRoutes");
 const userRoutes = require("../routes/userRoutes")
+const errorMiddleware = require("../middlewares/error")
 /* imported routes */
 app.use("/cars", productRoutes)
 app.use("/user", userRoutes)
+app.use(errorMiddleware)
 module.exports = app;
